69be2169ab9dece6ed3e3855057bdba1fb235dbe
oxc <oxc>
2018-03-05 14:50:06 +0800

    完成pc尾部

--- src/components/pc/pcIndex.js
+++ src/components/pc/pcIndex.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react'
 import PCHeader from './pcHeader'
-
+import PCFooter from './pcFooter'
 
 class PCIndex extends Component {
     render(){
         return (
             <div>
                 <PCHeader/>
+                <PCFooter/>
             </div>
         )
     }
