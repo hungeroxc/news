fa32dce484f95d15dd863a31df99abeb6e27b12b
oxc <oxc>
2018-03-06 11:23:52 +0800

    完成新闻详情页

---
+++ src/components/pc/pcNewsDetail.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+import {Row, Col, BackTop} from 'antd'
+import PCHeader from './pcHeader'
+import PCFooter from './pcFooter'
+import PCNewsImageBlock from './pcNewsImageBlock'
+
+
+class PCNewsDetails extends Component {
+    constructor(){
+        super()
+        this.state = {
+            newsItem: ''
+        }
+    }
+    componentWillMount(){
+        let p = this.props,
+            url = `http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=${p.match.params.uniquekey}`
+        axios.get(url)
+        .then(res => {
+            this.setState({
+                newsItem: res.data
+            })
+            if(!this.state.newsItem.title){
+                document.title = "OXC News | OXC 新闻平台"
+            }else{
+                document.title = this.state.newsItem.title + ' - OXC News | OXC 新闻平台'
+            }
+        })
+    }
+    createMarkup(){
+        return {
+            __html: this.state.newsItem.pagecontent
+        }
+    }
+    render(){
+        let html = this.createMarkup()
+        return (
+            <div>
+                <PCHeader></PCHeader>
+                <Row>
+                    <Col span={2}></Col>
+                    <Col className="container" span={14}>
+                        <div 
+                            className="articleContainer" 
+                            dangerouslySetInnerHTML={html}>
+                        </div>
+                        <br/>
+                        <hr/>
+
+                    </Col>
+                    <Col span={16}>
+                        <div style={{marginLeft: "200px"}}>
+                            <PCNewsImageBlock
+                                count={40}
+                                type="top"
+                                width="100%"
+                                cardTitle="相关新闻"
+                                imageWidth="160px"
+                            />
+                        </div>
+                    </Col>
+                    <Col span={2}></Col>
+                </Row>
+                <PCFooter/>
+                <BackTop/>
+            </div>
+        )
+    }
+}
+
+export default PCNewsDetails
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
+
 import 'antd/dist/antd.css'
+
+
 import PCIndex from './components/pc/pcIndex'
+import PCNewsDetails from './components/pc/pcNewsDetail'
 import './css/pc.css'
 
 
 
 ReactDOM.render(
     <div>
-        <PCIndex />
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={PCIndex}></Route>
+                <Route path="/details/:uniquekey" component={PCNewsDetails}></Route>
+            </Switch>
+        </BrowserRouter>
     </div>, 
     document.getElementById('root')
 )
